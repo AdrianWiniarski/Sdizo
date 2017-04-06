1b27bb31fcd925f1019370cd9b1ae92ee08ef3e9
AdrianWiniarski <winiak1996@gmail.com>
2017-04-06 14:24:22 +0200

    Update Kopiec.h

--- Kopiec.h
+++ Kopiec.h
@@ -1,8 +1,24 @@
-#pragma once
+#include "stdafx.h"
+#include <fstream>
+#include <iostream>
+#include <conio.h>
+#include <windows.h>
 class Kopiec
 {
+protected:
+	int rozmiar;
+	int* Tab;
+
 public:
-	Kopiec();
-	~Kopiec();
+	bool zaladujzpliku();
+	void dodajdokopca(int value);
+	void usunzkopca(int value);
+	bool czyjestwsrod(int value);
+	void wyswietlkopiec();
+
+
+	
+	//Kopiec();
+	//~Kopiec();
 };
 
