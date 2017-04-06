1939a098ca7914ee103bbad2b00fd46e9452ed61
AdrianWiniarski <winiak1996@gmail.com>
2017-04-06 14:24:52 +0200

    Update Lista.h

--- Lista.h
+++ Lista.h
@@ -1,8 +1,36 @@
 #pragma once
+#include "stdafx.h"
+#include <fstream>
+#include <iostream>
+#include <conio.h>
+#include <windows.h>
 class Lista
 {
 public:
-	Lista();
-	~Lista();
-};
 
+	int amount;
+
+	struct doubleList {
+
+		doubleList *next;
+		doubleList *prev;
+		int data;
+	};
+
+	doubleList *head;
+	doubleList *tail;
+	int counter;
+
+
+	int wielkosclisty();
+
+	void zaladujzplikulista();
+
+	bool czyjestwsrod(int wartosc);
+
+	void dodajdolisty(int index, int liczba);
+
+	void usunzlisty(int index);
+
+	void wyswietl();
+};
