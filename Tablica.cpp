248f651b1ecef2d6bb3f35d13e2e03bced6bfe2e
AdrianWiniarski <winiak1996@gmail.com>
2017-04-20 17:01:52 +0200

    Update Tablica.cpp

--- Tablica.cpp
+++ Tablica.cpp
@@ -5,7 +5,7 @@
 using namespace std;
 
 
-void Tablica::dodajliczbenak()
+void Tablica::dodajliczbenak(int liczba)
 {
 	rozmiar++;
 	int *Kopia = new int[rozmiar]; // tworze kopie tablicy liczb
@@ -16,13 +16,13 @@ void Tablica::dodajliczbenak()
 	}
 	delete[] Tab; // usuwam z pamieci
 	int a;
-	a = rand();
+	a = liczba;
 	Kopia[rozmiar - 1] = a;
 	Tab = Kopia;
 	Kopia = nullptr;
 };
 
-void Tablica::dodajliczbenap()
+void Tablica::dodajliczbenap(int liczba)
 {
 
 	rozmiar++;
@@ -34,7 +34,7 @@ void Tablica::dodajliczbenap()
 	}
 	delete[] Tab; // usuwam z pamieci
 	int a;
-	a = rand();
+	a = liczba;
 	Kopia[0] = a;
 	Tab = Kopia;
 	Kopia = nullptr;
@@ -88,53 +88,125 @@ void Tablica::usunliczbenak()
 
 };
 
-void Tablica::usunliczbelos()
+void Tablica::szukaj(int val)
 {
-	if (rozmiar != 0) {
-		int indeks;
-		if (rozmiar == 0)   indeks = 0;         //unikamy dzielenia przez zero
-		else indeks = rand() % rozmiar;
-
-		int *Kopia = new int[rozmiar - 1];
-		for (int i = 0; i < indeks; i++) {
-			Kopia[i] = Tab[i];                //przepisz wszystkie wartosci przed indeksem usuwanej wartosci
+	int x=0;
+	for (int i = 0; i < rozmiar; i++)
+	{
+		if(Tab[i]==val)
+		{
+			x = 1;
 		}
-		for (int i = 0; i < (rozmiar - 1); i++) {
-			Kopia[i] = Tab[i + 1];
-		}
-
-		delete[] Tab;
-		Tab = Kopia;
-		Kopia = nullptr;
-		rozmiar--;
+	};
+	if (x == 1)
+	{
+		system("cls");
+		cout << "W tablicy znajduje sie liczba " << val << endl;
+		Sleep(2000);
+		system("cls");
 	}
 	else
 	{
 		system("cls");
-		cout << endl << "Wielkosc tablicy jest rowna: " << rozmiar << "." << endl << "Zalecane: Stworz tablice." << endl;
-		Sleep(3000);
+		cout << "W tablicy nie ma liczby " << val << endl;
+		Sleep(2000);
 		system("cls");
 	}
 };
 
-void Tablica::dodajliczbelos()
+void Tablica::usunliczbelos(int index)
 {
-	//wybrane if (indeks >= rozmiar || indeks<0)    return false;       //sprawdz czy ktos nie podal za duzego indeksu
-	//wybrane else {
-	int liczba;
-	liczba = rand();
-	int indeks;
-	if (rozmiar == 0)   indeks = 0;         //unikamy dzielenia przez zero
-	else indeks = rand() % rozmiar;
+
+	//Sprawdź, czy tablica nie jest pusta
+	//Sprawdż czy pozycja nie wykracza poza tablicę
+	if (rozmiar) {
+		if (index > 0 & index < rozmiar++)
+		{
+			if (rozmiar > 0)
+			{
+				if(index==rozmiar)
+				{
+
+					rozmiar--;
+					int *Kopia = new int[rozmiar]; // tworze kopie tablicy liczb
+
+					for (int i = 0; i < rozmiar; i++) // nadpisuje kazdy element biorac obiekty z kopii (referencyjnie)
+					{
+						Kopia[i] = Tab[i];
+					}
+					delete[] Tab; // usuwam z pamieci
+					Tab = Kopia;
+					Kopia = nullptr;
+				}
+				else if (index > 0 || index < (rozmiar - 1))
+				{
+					index--;
+					//Zaalokuj pamięć na tablicę mniejszą o jeden element niż poprzednio
+					int *Kopia = new int[rozmiar - 1];
+
+					//Przepisz elementy do wybranej pozycji
+					for (int i = 0; i < index; i++) {
+						Kopia[i] = Tab[i];
+					}
+
+					rozmiar--;
+
+					//Przepisz elementy po wybranej pozycji
+					for (int i = index++; i < rozmiar; i++) {
+						Kopia[i] = Tab[i + 1];
+					}
+
+					delete[] Tab;
+					Tab = Kopia;
+
+					rozmiar--;
+					int *Kopia1 = new int[rozmiar]; // tworze kopie tablicy liczb
+
+					for (int i = 0; i < rozmiar; i++) // nadpisuje kazdy element biorac obiekty z kopii (referencyjnie)
+					{
+						Kopia1[i] = Tab[i];
+					}
+					delete[] Tab; // usuwam z pamieci
+					Tab = Kopia1;
+					Kopia1 = nullptr;
+					
+				}
+			}
+			else {
+				system("cls");
+				cout << "W tablicy nie istnieje pozycja " << index << endl;
+				Sleep(2000);
+				system("cls");
+			}
+		}
+
+	}
+};
+
+
+void Tablica::dodajliczbelos(int liczba, int index)
+{
+	index = index - 1;
+	if (index > rozmiar || index < 0)           //sprawdz czy ktos nie podal za duzego indeksu
+	{
+		system("cls");
+		cout << "Liczba znajduje sie poza zakresem tablicy" << endl;
+			Sleep(2000);
+	}
+	else {
+	
+	
+	if (rozmiar == 0)   index = 0;         //unikamy dzielenia przez zero
+	
 
 
 	int *Kopia = new int[rozmiar + 1];    //wskaznik na nowa o jeden wieksza tablcie
-	Kopia[indeks] = liczba;               //wstaw zadana liczbe na zadane miejsce
+	Kopia[index] = liczba;               //wstaw zadana liczbe na zadane miejsce
 
-	for (int i = 0; i < indeks; i++) {      //do miejsca wstawienia nowej wartosci
+	for (int i = 0; i < index; i++) {      //do miejsca wstawienia nowej wartosci
 		Kopia[i] = Tab[i];                //przepisz stare
 	}
-	for (int i = indeks; i < rozmiar; i++) {//kolejne tez przepisz
+	for (int i = index; i < rozmiar; i++) {//kolejne tez przepisz
 		Kopia[i + 1] = Tab[i];
 	}
 
@@ -142,18 +214,22 @@ void Tablica::dodajliczbelos()
 	Tab = Kopia;          //przypisz nowy wskaznik
 	Kopia = nullptr;
 	rozmiar++;              //zwieksz rozmiar o jeden
-    //return indeks;
-	//wybrane }
+  
+	
+	}
 };
 
 void Tablica::wypisztablice()
 {
-	system("cls");
-	for (int i = 0; i < rozmiar; i++)
+	if (rozmiar > 0)
 	{
-		cout << endl << i + 1 << ". " << Tab[i];
+		system("cls");
+		for (int i = 0; i < rozmiar; i++)
+		{
+			cout << endl << i + 1 << ". " << Tab[i];
+		}
+		cout << endl;
 	}
-	cout << endl;
 }
 void Tablica::zaladujzpliku()
 {
