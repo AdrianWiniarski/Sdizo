d5e0bf1c19afc99fb5b33a77a0f8945916f51ac7
AdrianWiniarski <winiak1996@gmail.com>
2017-04-20 17:00:44 +0200

    Update Lista.cpp

--- Lista.cpp
+++ Lista.cpp
@@ -1,123 +1,314 @@
 #include "stdafx.h"
 #include "Lista.h"
+#include <random>
 #include <fstream>
 #include <iostream>
 
 using namespace std;
-int Lista::wielkosclisty() {
 
-	return counter;
-
+Lista::Lista()
+{
+	rozmiar = 0;
+	poczatek = nullptr;
+	koniec = nullptr;
 }
 
-void Lista::zaladujzplikulista() {
+Lista::~Lista()
+{
+	poczatek = nullptr;
+	koniec = nullptr;
+	rozmiar = 0;
+}
 
-	head = tail = NULL;
-	doubleList* pom = new doubleList;
-	doubleList* tmp = new doubleList;
-	fstream plik;
-	plik.open("liczby.txt");
-	if (plik.good() == false)
-		cout << "Plik zostal zle wczytany";
-	plik >> amount;
-	for (int i = 0; i<amount; i++) {
+Element * Lista::getHead()
+{
+	return poczatek;
+}
 
-		tmp = new doubleList;
-		plik >> tmp->data;
-		tmp->prev = NULL;
-		tmp->next = NULL;
-		if (head == NULL)
-			head = tmp;
+void Lista::StworzL(int val)
+{
+	poczatek = new Element;
+	poczatek->wartosc = val;
+
+	poczatek->nastepny = nullptr;
+	poczatek->ostatni = nullptr;
+
+	koniec = poczatek;
+
+	rozmiar++;
+}
+
+int Lista::dodajWlos(int val,int indeks)
+{
+	if (indeks > 0 & indeks < rozmiar++)
+	{
+		if (poczatek == nullptr) //jesli lista pusta
+		{
+			StworzL(val);
+			return 0;
+		}
 		else
 		{
-			pom = head;
-			while (pom->next != NULL)
-				pom = pom->next;
-			tmp->prev = pom;
-			pom->next = tmp;
+			int index = indeks - 1;
+
+			if (index == 0) //przejdz do dodaj na poczatku
+			{
+				dodajWpocz(val);
+				return 0;
+			}
+
+			if (index == rozmiar) //przejdz do dodaj na koncu
+			{
+				dodajWkon(val);
+				return index;
+			}
+
+			Element* newElement = new Element;
+			newElement->wartosc = val;
+
+			Element* temp = znajdzL(index);
+
+			newElement->nastepny = temp;
+			newElement->ostatni = temp->ostatni;
+
+			temp->ostatni = newElement;
+			newElement->ostatni->nastepny = newElement;
+
+			rozmiar++;
+
+			return index;
 		}
-		counter++;
 	}
-	tail = tmp;
+	else
+	{
+		system("cls");
+		cout << endl << "Index spoza przedzialu sprobuj ponownie " << endl;
+		Sleep(2000);
+		system("cls");
+	}
 }
 
-bool Lista::czyjestwsrod(int val) {
+void Lista::dodajWpocz(int val)
+{
+	if (poczatek==nullptr) //jesli lista pusta
+		StworzL(val);
+	else
+	{
+		poczatek->ostatni = new Element;
+		poczatek->ostatni->wartosc = val;
+		poczatek->ostatni->nastepny = poczatek;
+		poczatek->ostatni->ostatni = nullptr;
+		if (rozmiar == 1)
+			koniec = poczatek;
+		poczatek = poczatek->ostatni;
+		rozmiar++;
+	}
+}
 
-	doubleList* e;
-	e = head;
-	while (e) {
-		if (e->data == val)
-			return true;
-		e = e->next;
+void Lista::dodajWkon(int val)
+{
+	if (poczatek == nullptr) //jesli lista pusta
+		StworzL(val);
+	else
+	{
+		koniec->nastepny = new Element;
+		koniec->nastepny->wartosc = val;
+		koniec->nastepny->ostatni = koniec;
+		koniec->nastepny->nastepny = nullptr;
+		if (rozmiar == 1)
+			poczatek = koniec;
+		koniec = koniec->nastepny;
+		rozmiar++;
+	}
+}
+
+int Lista::usunWlos(int indeks)
+
+{
+	if (indeks > 0 & indeks < rozmiar++)
+	{
+		if (rozmiar > 0)
+		{
+			if (indeks > 0 || indeks < (rozmiar - 1)) {
+				int index = indeks - 1;
+
+				if (index == 0) //przejdz do usun na poczatku
+				{
+					usunWpocz();
+					return 0;
+				}
+
+				if (index == rozmiar - 1) //przejdz do usun na koncu
+				{
+					usunWkon();
+					return index;
+				}
+
+				Element* temp = znajdzL(index);
+
+				temp->ostatni->nastepny = temp->nastepny;
+				temp->nastepny->ostatni = temp->ostatni;
+				delete temp;
+				rozmiar--;
+
+				return index;
+			}
+			else
+			{
+				system("cls");
+				cout << endl << "Index spoza przedzialu sprobuj ponownie " << endl;
+				Sleep(2000);
+				system("cls");
+			}
+		}
+	}
+	else
+	{
+		system("cls");
+		cout << endl << "Index spoza przedzialu sprobuj ponownie " << endl;
+		Sleep(2000);
+		system("cls");
+	}
+}
+void Lista::usunWpocz()
+{
+	if (rozmiar)
+	{
+		if (rozmiar == 1)
+			clear();
+		else
+		{
+			poczatek = poczatek->nastepny;
+			delete poczatek->ostatni;
+			poczatek->ostatni = nullptr;
+			rozmiar--;
+		}
+	}
+}
+
+void Lista::usunWkon()
+{
+	if (rozmiar)
+	{
+		if (rozmiar == 1)
+			clear();
+		else
+		{
+			koniec = koniec->ostatni;
+			delete koniec->nastepny;
+			koniec->nastepny = nullptr;
+			rozmiar--;
+		}
+	}
+}
+
+Element * Lista::znajdzL(int index)
+{
+	Element* temp;
+
+	if (index < rozmiar / 2)
+	{
+		temp = poczatek; //liczenie od head do przodu
+
+		for (int i = 0; i < index; i++)
+			temp = temp->nastepny;
+	}
+	else
+	{
+		temp = koniec; //liczenie od tail w tyl
+
+		for (int i = rozmiar - 1; i > index; i--)
+			temp = temp->ostatni;
+	}
+	return temp;
+}
+
+int Lista::znajdzWart(int val)
+{
+	Element* element = poczatek; //wskaznik na aktualny element
+
+	for (int i = 0; i < rozmiar; i++)
+	{
+		if (element->wartosc == val)
+		return	true;
+		else
+			element = element->nastepny;
 	}
 	return false;
 }
 
-void Lista::dodajdolisty(int index, int liczba) {
+bool Lista::zaladujzplikuLista()
+{
+	std::fstream file;
+	int read; //liczby zczytane z pliku
+	int readSize; //ilosc elementow zczytana z pliku
 
-	doubleList* p = new doubleList;
-	doubleList* n = new doubleList;
-	doubleList* e = new doubleList;
-	e->data = liczba;
-	if (index == 1) {
-		e = head->prev;
-		e->prev = NULL;
-		head = e;
-	}
-	else {
-		if (head)
-			p = head;
-		for (int i = 0; i<index - 1; i++)
-			p = p->next;
-		n = p;
-		n = n->prev;
-		p->prev = e;
-		n->next = e;
-		e->prev = n;
-		e->next = p;
-	}
-	counter++;
-}
+	file.open("Liczby.txt");
 
-void Lista::usunzlisty(int index) {
+	if (file.is_open())
+	{
+		file >> readSize;
 
-	doubleList* e = new doubleList;
-	doubleList* p = new doubleList;
-	doubleList* n = new doubleList;
-	if (head) {
-		e = head;
-		if (index<amount) {
-			for (int i = 0; i<index - 1; i++)
-				e = e->next;
-			if (e == NULL) {
-				e = tail;
-				tail = tail->prev;
-				e = NULL;
-				return;
-			}
-			p = e->prev;
-			n = e->next;
-			p->next = e->next;
-			n->prev = e->prev;
-			e = NULL;
-			counter--;
-			return;
-		}
+		if (file.fail())
+			return false;
 		else
-			cout << "Zbyt duzy indeks";
+		{
+			for (int i = 0; i < readSize; i++)
+			{
+				file >> read;
+				if (file.fail())
+					return false;
+				else
+					dodajWkon(read);
+			}
+		}
+		file.close();
+		return true;
 	}
 	else
-		cout << "Brak listy";
+		return false;
 }
 
-void Lista::wyswietl() {
-	system("cls");
-	int i = 1;
-	doubleList* e;
-	e = head;
-	while (e) {
-		cout << i << ". " << e->data << endl;
-		e = e->next;
-		i++;
+
+void Lista::wyswietlListe()
+{
+	Element* element = poczatek; //wskaznik na aktualnie wypisywany element
+	int columnCounter = 0; //licznik kolumn
+    system("cls");
+	
+	for (int i = 0; i < rozmiar; i++)
+	{
+		
+		cout<<i+1<<". " << element->wartosc << endl;
+		element = element->nastepny;
+		if (++columnCounter == 20)
+		{
+			cout << endl;
+			columnCounter = 0;
+		}
 	}
-};
+}
+
+void Lista::clear()
+{
+	poczatek = nullptr;
+	koniec = nullptr;
+	rozmiar = 0;
+}
+void Lista::czyJest(int value)
+{
+	if (znajdzWart(value) == true)
+	{
+		system("cls");
+		cout<<endl << "Wartosc- "<<value<<", znajduje sie w liscie" << endl;
+		Sleep(2000);
+		system("cls");
+	}
+	else
+	{
+		system("cls");
+		cout << endl << "Wartosc- " << value << ", nie znajduje sie w liscie" << endl;
+		Sleep(2000);
+		system("cls");
+	}
+}
